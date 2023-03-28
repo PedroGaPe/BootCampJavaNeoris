e07500e1c6903493a03d27f050c4011d4e8f9e4d
p-pgarre <p-pgarre@P-PGARRE.neoris.cxnetworks.net>
2023-03-28 15:30:04 +0200

    Dibujar cuadrado, tablas de multiplicar y primos

---
+++ prueba1/src/prueba1/DibujarCuadrado.java
@@ -0,0 +1,16 @@
+package prueba1;
+
+public class DibujarCuadrado {
+
+	public static void main(String[] args) {
+		int size = 8;
+		
+		for(int i=1; i<=size;i++) {
+			for(int j=1; j<=size;j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+
+}
