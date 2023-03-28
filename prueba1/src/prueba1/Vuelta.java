f2b091206e8ffd884b27625f41bdb23b022b1c37
p-pgarre <p-pgarre@P-PGARRE.neoris.cxnetworks.net>
2023-03-28 11:44:12 +0200

    ejercicio vuelta

--- prueba1/src/prueba1/Vuelta.java
+++ prueba1/src/prueba1/Vuelta.java
@@ -15,7 +15,53 @@ public class Vuelta {
 		for(int i = longuitud-1; i>=0;i--) {
 			System.out.print(entrada.charAt(i));
 		}
-		
+		System.out.println();
+		String entradaInversa="";
+		for (int i = longuitud-1; i>=0;i--) {
+			entradaInversa+=entrada.charAt(i);
+		}
+		System.out.println("fase-3-"+entradaInversa);
+		String entradaInversaNums="";
+		for (int i= longuitud-1; i>=0;i--) {
+			char aux = entrada.charAt(i);
+			switch(aux) {
+			case 'a':
+				entradaInversaNums+=4;
+				break;
+			case 'e':
+				entradaInversaNums+=3;
+				break;
+			case 'i':
+				entradaInversaNums+=1;
+				break;
+			case 'o':
+				entradaInversaNums+=0;
+				break;
+			case 'u':
+				entradaInversaNums+=9;
+				break;
+			default:
+				entradaInversaNums+=aux;
+				break;
+				
+			}
+		}
+		System.out.println("fase-4-"+entradaInversaNums);
+		char primero = entrada.charAt(0);
+		int ultimo = entrada.lastIndexOf(primero);
+		System.out.print("fase-5-");
+		for(int i = ultimo-1;i>=0;i--) {
+			System.out.print(entrada.charAt(i));
+		}
+		System.out.println();
+		int indice =0;
+		System.out.print("fase-6-");
+		do {
+			System.out.print(entrada.charAt(indice));
+			indice++;
+			
+		}while(entrada.charAt(indice)!=primero);
+		System.out.println();
 	}
 
 }
